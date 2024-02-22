3ec6e41b4f358e1a65f58f14d4211b0aea4cab87
Banuprakash <banuprakashc@yahoo.co.in>
2024-02-22 11:09:16 +0530

    day 4

--- workspace/orderapp/src/main/java/com/adobe/orderapp/client/OrderClient.java
+++ workspace/orderapp/src/main/java/com/adobe/orderapp/client/OrderClient.java
@@ -9,6 +9,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @AllArgsConstructor
 //@org.springframework.core.annotation.Order(1)
@@ -16,15 +18,28 @@ public class OrderClient implements CommandLineRunner {
     private final OrderService service;
     @Override
     public void run(String... args) throws Exception {
-        newOrder();
+      //  newOrder();
+        getOrders();
+    }
+
+    private void getOrders() {
+        List<Order> orders = service.getOrders();
+        for(Order order : orders) {
+            System.out.println(order.getOid() +" : " + order.getCustomer().getFirstName() + " : " + order.getTotal() );
+            for(Item item : order.getItems()) { // LazyInitializationException
+                System.out.println(item.getProduct().getName() +" ," + item.getQuantity() + ", " + item.getAmount());
+            }
+
+            System.out.println("Total : " + order.getTotal());
+        }
     }
 
     private void newOrder() {
         Order order = new Order();
-        order.setCustomer(Customer.builder().email("swetha@adobe.com").build());
+        order.setCustomer(Customer.builder().email("raj@adobe.com").build());
 
-        Item i1 = Item.builder().product(Product.builder().id(2).build()).quantity(2).build();
-        Item i2 = Item.builder().product(Product.builder().id(1).build()).quantity(3).build();
+        Item i1 = Item.builder().product(Product.builder().id(1).build()).quantity(2).build();
+        Item i2 = Item.builder().product(Product.builder().id(2).build()).quantity(1).build();
         order.getItems().add(i1);
         order.getItems().add(i2);
 
--- workspace/orderapp/src/main/java/com/adobe/orderapp/service/OrderService.java
+++ workspace/orderapp/src/main/java/com/adobe/orderapp/service/OrderService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @AllArgsConstructor
